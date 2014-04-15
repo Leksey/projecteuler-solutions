0d12d5ebcaff7af6cc6fbc6ecc7bf717f37216d9
Leksey <leksey@leksey.com>
2014-04-16 01:17:33 +0400

    #009

---
+++ p009.py
@@ -0,0 +1,8 @@
+for i in range(1,n+1):
+    j = 1
+    while i > j:
+        if j ** 2 + i ** 2 == (1000 - j - i) ** 2:
+            print i*j*(1000 - j - i)
+            break
+        else:
+            j += 1
